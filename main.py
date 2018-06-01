4aad18674d7a0e4d05e83851ad46141a8804a469
Lucas Abbade <lucasabbade@Lucass-MacBook-Pro.local>
2018-06-01 00:36:54 -0300

    cleaning up

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from api_token import get_token
 import discord
 from discord.ext import commands
 
+song_url = 'https://www.youtube.com/watch?v=ZZ5LpwO-An4'
+
 bot = commands.Bot(command_prefix='$', description='I play he man forever')
 
 @bot.event
@@ -12,14 +14,14 @@ async def on_ready():
     print('------')
 
 @bot.command()
-async def cat(ctx):
-    await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
-
-@bot.command()
-async def info(ctx):
+async def info():
     embed = discord.Embed(title="He-Man bot", description="Plays Hey Yeah Yeah by He Man", color=0x146eff)
     embed.add_field(name="Author", value="[LRAbbade](http://www.github.com/LRAbbade)")
     embed.add_field(name="Source code", value="[He_Man_Bot](http://www.github.com/LRAbbade/He_Man_Bot)")
-    await ctx.send(embed=embed)
+    await bot.say(embed=embed)
+
+@bot.command()
+async def start():
+    await bot.say('HEYYEYAAEYAAAEYAEYAA')
 
 bot.run(get_token())
